2f6efbaa3f0acff4950d40761667c354c41c7db8
Toni Kousek <toni.kousek@gmail.com>
2024-03-05 14:23:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Guessr
+
+A game where you have to guess a word based on the description of it
+
+# Public APIs
+
+## Random word API
+
+https://random-word-api.herokuapp.com/home
+
+## Free dictionary API
+https://dictionaryapi.dev/
---
+++ app/src/main/java/com/kousek/toni/guessr/MainActivity.java
@@ -0,0 +1,18 @@
+package com.kousek.toni.guessr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    static void getRandomWord(){
+        
+    }
+}
